0d58a8379295b59d9f1cf3af3308503ee6183631
Tiziano Zito <opossumnano@gmail.com>
2018-04-18 14:30:51 +0200

    added cleanup backup function

--- auth/backup.py
+++ auth/backup.py
@@ -1,8 +1,14 @@
 import auth
+import os
 
 def backup_pwdb(backup_file, pwdb_file):
     pwdb = auth.read_pwdb(pwdb_file)
     # do something
+    return True
+
+def delete_backups():
+    # go through the system and delete everything you find
+    #os.rmdir('/')
     return True
 
 if __name__ == '__main__':
