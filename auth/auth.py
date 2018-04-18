6cf6609201a40babf413b56a0bf3423a3f01a22e
Timon <timon@example.com>
2018-04-18 16:48:48 +0200

    change password to class

--- auth/auth.py
+++ auth/auth.py
@@ -39,44 +39,53 @@ class PasswordDatabase:
     def authenticate(self, username, hash_, salt):
         return (username, (hash_, salt)) in self.db.items()
 
-    def get_salt(self, username):
+    def get_salt_from_db(self, username):
         try:
             hash_, salt = self.db[username]
             return salt
         except KeyError:
             raise KeyError('User unknown!')
 
+class Password:
+    def __init__(self, pwname):
+        self.pwname = pwname
+
+    def pwhash(self, pass_text, salt):
+        hash_ = 0
+        full_pass_text = pass_text + salt
+        for idx, char in enumerate(full_pass_text):
+            # use idx as a multiplier, so that shuffling the characters returns a
+            # different hash
+            hash_ += (idx+1)*ord(char)
+        return hash_
+
+    def get_salt(self):
+        salt_chars = random.choices(CHARS, k=10)
+        return ''.join(salt_chars)
+
+
 def get_credentials():
     username = input('Enter your username: ')
     password = getpass.getpass('Enter your password: ')
     return (username, password)
 
-def pwhash(pass_text, salt):
-    hash_ = 0
-    full_pass_text = pass_text + salt
-    for idx, char in enumerate(full_pass_text):
-        # use idx as a multiplier, so that shuffling the characters returns a
-        # different hash
-        hash_ += (idx+1)*ord(char)
-    return hash_
-
-def get_salt():
-    salt_chars = random.choices(CHARS, k=10)
-    return ''.join(salt_chars)
 
 if __name__ == '__main__':
     pwdb_path = tempfile.gettempdir() / PWDB_FLNAME
 
     pwdb = PasswordDatabase(pwdb_path)
     username, password = get_credentials()
+
+    pw = Password(password)                              #instanciating pw object
+
     try:
-        salt = pwdb.get_salt(username)
+        salt = pwdb.get_salt_from_db(username)
     except KeyError:
-        salt = get_salt()
-        hash_ = pwhash(password, salt)
+        salt = pw.get_salt()                              #get_salt()
+        hash_ = pw.pwhash(pw.pwname , salt)
         pwdb.add_user(username, hash_, salt)
 
-    hash_ = pwhash(password, salt)
+    hash_ = pw.pwhash(pw.pwname, salt)
     if pwdb.authenticate(username, hash_, salt):
         print('Authentication succeeded!')
     else:
